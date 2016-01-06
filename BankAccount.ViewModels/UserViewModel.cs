86d4ca73995ecb62eb7b794afda12121ec8234be
Aleksandar Ristic <aleksandar.r@gmx.at>
2016-01-06 01:54:34 +0100

    added validation to login model

--- BankAccount.ViewModels/UserViewModel.cs
+++ BankAccount.ViewModels/UserViewModel.cs
@@ -5,10 +5,12 @@ namespace BankAccount.ViewModels
     public class UserViewModel
     {
         [Required]
+        [MinLength(4)]
         [Display(Name = "Employee ID")]
         public string EmployeeId { get; set; }
 
         [Required]
+        [MinLength(4)]
         [Display(Name = "User name")]
         public string UserName { get; set; }
 
